6589b613786de6bfe4b2a710d4b1973c2d9b845a
Aleem Isiaka <aleemisiaka@gmail.com>
2019-07-22 22:00:45 +0100

    fix: ready for server rendering

--- dist/es/index.js
+++ dist/es/index.js
@@ -2,5 +2,10 @@ import WebworkerClient from "./FFMPEGWebWorkerClient";
 import Webworker from "./FFMPEGWebWorker";
 export var FFMPEGWebworker = Webworker;
 export var FFMPEGWebworkerClient = WebworkerClient;
-var workerClient = new WebworkerClient();
+var workerClient = WebworkerClient;
+
+if (window && window.Blob) {
+  workerClient = new WebworkerClient();
+}
+
 export default workerClient;
--- src/index.js
+++ src/index.js
@@ -4,7 +4,8 @@ import Webworker from "./FFMPEGWebWorker";
 export const FFMPEGWebworker = Webworker;
 export const FFMPEGWebworkerClient = WebworkerClient;
 let workerClient = WebworkerClient;
-if (window && window.Blob) {
+const _window = global || window;
+if (_window && _window.Blob) {
   workerClient = new WebworkerClient();
 }
 export default workerClient;
