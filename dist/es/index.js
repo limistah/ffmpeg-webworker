5288603431e69a29115d91a0dfba08f0b338f361
Aleem Isiaka <aleemisiaka@gmail.com>
2019-07-23 08:57:10 +0100

    build: fix

--- dist/es/index.js
+++ dist/es/index.js
@@ -2,7 +2,10 @@ import WebworkerClient from "./FFMPEGWebWorkerClient";
 import Webworker from "./FFMPEGWebWorker";
 export var FFMPEGWebworker = Webworker;
 export var FFMPEGWebworkerClient = WebworkerClient;
-var workerClient = WebworkerClient;
+var workerClient = {
+  on: function on() {},
+  emit: function emit() {}
+};
 
 var _window = global || window;
 
